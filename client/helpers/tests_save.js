2e44e94bac95847c1533f3a02b98f09f55348968
nigel <nwre87@gmail.com>
2014-09-09 17:19:31 +0800

    rough category results

--- client/helpers/tests_save.js
+++ client/helpers/tests_save.js
@@ -1,49 +1,28 @@
 processForm = function(testId, formName, category) {
   var saveResult = function (testId, category, subcategory, hash) {
-      // resultObj = {};
-      // resultObj[category] = {};
-      // resultObj[category + "." + subcategory] = result ;
-      // console.log(resultObj);
-      // Tests.update(testId, { $set: resultObj });
-
-    // Tests.update(testId, {$set: {"phoneInbound.greeting": hash} } );
     objectToInsert = {};
     objectToInsert[category] = {};
     objectToInsert[category][subcategory] = hash;
-    console.log(objectToInsert);
     insertionTarget = category + "." + subcategory;
-
     var setHash = { $set: {} };
-
-    // setHash.$set.phoneInbound.greeting = hash;
     setHash.$set[insertionTarget] = hash;
-    console.log(setHash);
     Tests.update(testId, setHash);
   }
 
   var selector = 'form[name=\"'+formName+'\"]';
   var form = $(selector).serializeObject();
-  // greeting
   var subcategory = Object.keys(form)[0];
-  // greeting object
   var result = form[subcategory];
   var hash = {};
-  var average = subCatAverage(result);
+  var average = subCatResult(result);
   hash = {};
   hash.test = result;
-  hash.test_average = average;
-  console.log(hash);
-  // hash[subcategory] = {};
-  // hash[subcategory]["test"] = result;
-  // hash[subcategory]["test_average"] = average;
-  // hash will be greeting object with scores
-  // category will be "phoneInbound"
-  // subcategory will be "greeting"
+  hash.sub_cat_result = average;
   saveResult(testId, category, subcategory, hash);
 }
 
 
-subCatAverage = function(result) {
+subCatResult = function(result) {
   var totalScore = 0;
   var totalMaxScore = 0;
   for(var i = 0; i < result.length; i++){
@@ -55,7 +34,10 @@ subCatAverage = function(result) {
       totalMaxScore += maxScore;
     }
   }
-  return totalScore / totalMaxScore * 100
+  var scoresTotal = {};
+  scoresTotal.total_score = totalScore;
+  scoresTotal.max_score = totalMaxScore;
+  return scoresTotal;
 }
 
 
