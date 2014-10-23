742f090d439b4a0c44d451bb8258f6fceb7d647f
nigel <nwre87@gmail.com>
2014-10-23 20:37:39 +0800

    rough previous population of radio buttons and unclick of radio buttons

--- client/views/test/phone inbound/greeting.js
+++ client/views/test/phone inbound/greeting.js
@@ -5,18 +5,36 @@ Template.greeting.events({
 
     var redirectUrl = "/" + this._id + "/call_management";
     Router.go(redirectUrl);
-
-
-  // }
   }
 });
 
-// Template.greeting.rendered = function(){
+Template.greeting.rendered = function(){
+  unclickRadioButtons();
 
-//     console.log(this.data);
-//     testContents = this.data;
-//     if (!!testContents.phoneInbound){
-//         q1Score = testContents.phoneInbound.greeting.q1.score;
-//         $('input[name="q1[score]"][value=' + q1Score + ']').attr("checked", "true");
-//     }
-// };
+  testContents = Tests.find({_id: "mWbGLHxN4Z7jN6E9u"}, {fields: {categories: 1, _id: 0}}).fetch()[0];
+  console.log('testContents');
+  console.log(testContents);
+  for(var key in testContents){
+    obj = testContents[key];
+    console.log('obj: ', obj);
+    for(var prop in obj){
+      yup = obj[prop].greeting;
+      console.log('yuppers', yup.test);
+      test = yup.test
+      console.log('test', test);
+    }
+    // return test
+  }
+
+  // loop = function(subCat){
+    for(var i = 0; i < test.length; i++){
+      console.log('i', i);
+      score = test[i].score;
+      console.log('score: ', score);
+      var questions = $('input[name="greeting' + '[' + i + '][score]"][value=' + score + ']').attr("checked", "true");
+      console.log('questions: ', questions);
+    }
+    return questions;
+  // }
+  // loop("greeting");
+};
