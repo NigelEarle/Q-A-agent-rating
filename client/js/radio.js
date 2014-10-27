a7b8b929b8087ac6ef7f112104f28307e10b7a15
nigel <nwre87@gmail.com>
2014-10-27 18:49:15 +0800

    dynamic test data generating working.  Still error with subCat on generateTestData function

--- client/js/radio.js
+++ client/js/radio.js
@@ -11,6 +11,29 @@ unclickRadioButtons = function(){
         booRadio = this;
       }
     };
-
   }
+}
+
+findCurrentTestData = function(thisData, category){
+  var data = thisData;
+  console.log('data: ', data);
+  var nestedData = data.categories[category];
+  console.log('nestedData: ', nestedData);
+  for(var key in nestedData){
+    var objectArray = nestedData[key].test;
+    console.log('test: ', objectArray);
+  }
+  return objectArray;
+}
+
+generateTestData = function(questionsArray, subCat){
+  console.log('questionsArray: ', questionsArray.length);
+  for(var i = 0; i < questionsArray.length; i++){
+    console.log('i', i);
+    score = questionsArray[i].score;
+    console.log('score: ', score);
+    var questions = $('input[name="subCat' + '[' + i + '][score]"][value=' + score + ']').attr("checked", "true");
+    console.log('questions: ', questions);
+  }
+  return questions;
 }
--- client/views/test/phone inbound/greeting.js
+++ client/views/test/phone inbound/greeting.js
@@ -10,31 +10,6 @@ Template.greeting.events({
 
 Template.greeting.rendered = function(){
   unclickRadioButtons();
-
-  testContents = Tests.find({_id: "mWbGLHxN4Z7jN6E9u"}, {fields: {categories: 1, _id: 0}}).fetch()[0];
-  console.log('testContents');
-  console.log(testContents);
-  for(var key in testContents){
-    obj = testContents[key];
-    console.log('obj: ', obj);
-    for(var prop in obj){
-      yup = obj[prop].greeting;
-      console.log('yuppers', yup.test);
-      test = yup.test
-      console.log('test', test);
-    }
-    // return test
-  }
-
-  // loop = function(subCat){
-    for(var i = 0; i < test.length; i++){
-      console.log('i', i);
-      score = test[i].score;
-      console.log('score: ', score);
-      var questions = $('input[name="greeting' + '[' + i + '][score]"][value=' + score + ']').attr("checked", "true");
-      console.log('questions: ', questions);
-    }
-    return questions;
-  // }
-  // loop("greeting");
+  var questionsArray = findCurrentTestData(this.data, "phoneInbound");
+  generateTestData(questionsArray, "greeting");
 };
